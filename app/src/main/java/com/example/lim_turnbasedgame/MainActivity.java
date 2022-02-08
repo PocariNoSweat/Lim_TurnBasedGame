a17d050f71095ff9f8b08ecaf779a7c52f872186
PocariNoSweat <christianlim0031@gmail.com>
2022-02-08 22:48:25 +0900

    dili nako

--- app/src/main/java/com/example/lim_turnbasedgame/MainActivity.java
+++ app/src/main/java/com/example/lim_turnbasedgame/MainActivity.java
@@ -3,30 +3,51 @@ package com.example.lim_turnbasedgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtCharName,txtEnemyName,txtCharHP,txtEnemyHP,txtCharMP,txtEnemyMP;
+    Button btnEndTurn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //XML ids for text and button
+        txtCharName = findViewById(R.id.txtCharName);
+        txtEnemyName = findViewById(R.id.txtEnemyName);
+        txtCharHP = findViewById(R.id.txtCharHP);
+        txtEnemyMP = findViewById(R.id.txtEnemyMP);
+        txtCharMP = findViewById(R.id.txtCharMP);
+        txtEnemyMP = findViewById(R.id.txtEnemyMP);
+        btnEndTurn = findViewById(R.id.btnEndTurn);
 
-        //Methods can ask for values and give a return value except for void method types
 
-        //Scenario you have a program that gives you a value of 9 if you provide it a string "Ebargs"
+        String CharName = "Aleg";
+        int heroHP = 1000;
+        int heroMP = 500;
+        int heroMinDamage = 80;
+        int heroMaxDamage = 130;
+
+        String EnemyName = "Midget Slayer";
+        int enemyHP = 2000;
+        int enemyMP = 200;
+        int enemyMinDamage = 30;
+        int enemyMaxDamage = 50;
+
+        txtCharName.setText(CharName);
+        txtCharHP.setText(String.valueOf(heroHP));
+        txtCharMP.setText(String.valueOf(heroMP));
+
+        txtEnemyName.setText(EnemyName);
+        txtEnemyHP.setText(String.valueOf(enemyHP));
+        txtEnemyMP.setText(String.valueOf(enemyMP));
 
 
-        value("Ebargs");
 
-    }
 
-    int value(String x){
-
-        if(x=="Ebargs"){
-            return 9;
-
-        }
-
-        return 1;
     }
 }
